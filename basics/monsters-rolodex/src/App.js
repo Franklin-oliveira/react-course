51a871e9704ca92496e0b7075c6000cbea2b3f99
Franklin Oliveira <franklin.alves.oliveira@gmail.com>
2021-04-11 12:32:01 -0300

    Started to modify the base project to create a list of monsters - Monsters Rolodex

--- basics/monsters-rolodex/src/App.js
+++ basics/monsters-rolodex/src/App.js
@@ -7,21 +7,32 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "Hello Franklin!"
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.string}
-        </p>
-        <button onClick={() => this.setState({ string:"This is awesome!" })}>Change Text</button>
-      </header>
-    </div>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key= {monster.id} >{ monster.name }</h1>
+          ))
+        }
+      </div>
     )
   }
 }
