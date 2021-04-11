eee2d6ddad78e0d88fa0393418bd3de7d8e664bf
Franklin Oliveira <franklin.alves.oliveira@gmail.com>
2021-04-11 16:51:18 -0300

    Added cards components

--- basics/monsters-rolodex/src/App.js
+++ basics/monsters-rolodex/src/App.js
@@ -23,12 +23,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name='Justiniano'>
-          {
-            this.state.monsters.map(monster => (
-              <h1 key= {monster.id} >{ monster.name }</h1>
-            ))
-          }
+        <CardList monsters={this.state.monsters}>
+
         </CardList>
       </div>
     )
