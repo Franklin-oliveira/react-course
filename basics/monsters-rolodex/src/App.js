0b6788459527113fc6fb2ab913a17be612fd748c
Franklin Oliveira <franklin.alves.oliveira@gmail.com>
2021-04-30 15:24:06 -0300

    criei componente search-box

--- basics/monsters-rolodex/src/App.js
+++ basics/monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
-import {CardList} from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 import './App.css';
 
@@ -18,27 +19,23 @@ class App extends Component {
     // fetching data from an API (BackEnd)
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({ monsters: users}))
+      .then(users => this.setState({ monsters: users }))
   }
 
   render() {
-    
+
     // filtering monsters array based on the search box
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => (
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     ))
-    
+
 
     return (
       <div className="App">
         {/* to ckeck the values right away, add this second argument in the second parameter of setState: () => console.log(this.state)*/}
-        <input 
-        type='search' 
-        placeholder='search monsters' 
-        onChange={e => 
-          this.setState({searchField: e.target.value})}
-        />
+        <SearchBox placeholder='search monsters' handleChange={e =>
+          this.setState({ searchField: e.target.value })} />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
     )
