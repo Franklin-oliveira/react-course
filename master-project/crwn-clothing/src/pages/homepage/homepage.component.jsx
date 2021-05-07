0ccbca47536a18e1173f12e21cb184f769515064
Franklin Oliveira <franklin.alves.oliveira@gmail.com>
2021-05-07 16:59:24 -0300

    broken down the homepage into smaller components and added more styling

---
+++ master-project/crwn-clothing/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+// Components
+import Directory from '../../components/directory/directory.component'
+// import MenuItem from '../../components/menu-item/menu-item.component'
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+);
+
+export default HomePage;
