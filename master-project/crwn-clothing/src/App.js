d78a126c8f387ea5a2e3ec102ae75b39a6dc372f
Franklin Oliveira <franklin.alves.oliveira@gmail.com>
2021-05-08 12:11:25 -0300

    made a shop page which shows collection items preview

--- master-project/crwn-clothing/src/App.js
+++ master-project/crwn-clothing/src/App.js
@@ -4,20 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
 
-const  HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact={true} path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
+        {/* <Route path='/hats' component={HatsPage}/> */}
       </Switch>
     </div>
   );
