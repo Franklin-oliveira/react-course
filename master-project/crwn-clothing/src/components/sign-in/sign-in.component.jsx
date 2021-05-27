d350445f0fbdfc6bc1552946345efe8bbdf1ea85
Franklin Oliveira <franklin.alves.oliveira@gmail.com>
2021-05-27 17:26:11 -0300

    criei componente para botão estilizado

--- master-project/crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ master-project/crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -4,6 +4,7 @@ import './sign-in.styles.scss';
 
 // Components
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends Component {
     constructor(props) {
@@ -51,7 +52,7 @@ class SignIn extends Component {
                         required 
                     />
 
-                    <input type="submit" value="Submit Form" />
+                    <CustomButton type="submit">Sign in</CustomButton>
                 </form>
             </div>
         )
